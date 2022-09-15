3b54ea06eb22aa8ac4750054cbc49bed7e66ee4a
Sushmi-3 <sushmisumi3@gmail.com>
2022-09-15 01:35:04 -0700

    usestate with arrays and object

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import StateCounter from './components/StateCounter';
 import StateFunc from './components/StateFunc';
+import StateWObject from './components/StateWObject';
 import SetStateWPrev from './components/SetStateWPrev';
+import StateWArray from './components/StateWArray';
 function App() {
   return (
     <div className="App">
       <StateCounter></StateCounter>
       <StateFunc></StateFunc>
       <SetStateWPrev/>
+      <StateWObject></StateWObject>
+      <StateWArray></StateWArray>
     </div>
   );
 }
