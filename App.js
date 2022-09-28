d88f788c3dda75557684d0fdb3ccb73e13f403bf
Sushmi-3 <101321632+Sushmi-3@users.noreply.github.com>
2022-09-28 03:21:07 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import PostList from './components/PostList';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <div className="App">
+    {/* <PostList></PostList> */}
+    <Form></Form>
+    <PostList></PostList>
+        
+    </div>
+  );
+}
+
+export default App;
