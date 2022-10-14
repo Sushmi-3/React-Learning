b7e3553b021575a4f6c14a9045952f6a7ccb6395
Sushmi-3 <sushmisumi3@gmail.com>
2022-10-14 09:01:15 -0700

    useEffect ,useReducer,useRef

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import EffectRender from './components/EffectRender'
+import UseCounter from './components/UseCounter';
+import UseReducer from './components/UseReducer';
+import UseRef from './components/UseRef';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <EffectRender></EffectRender>
+      <UseCounter></UseCounter>
+      <UseReducer></UseReducer>
+      <UseRef></UseRef>
     </div>
+
   );
 }
 
